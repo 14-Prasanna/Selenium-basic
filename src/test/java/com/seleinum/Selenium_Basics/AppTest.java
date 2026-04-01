5b8675a2b59d8021b09ab02d8e3731b8c1675df7
14-Prasanna <2k22cse115@kiot.ac.in>
2026-04-01 11:47:50 +0530

    The Pom.xml

--- src/test/java/com/seleinum/Selenium_Basics/AppTest.java
+++ src/test/java/com/seleinum/Selenium_Basics/AppTest.java
@@ -3,13 +3,17 @@ package com.seleinum.Selenium_Basics;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class AppTest {
 
     @Test
     public void runFlipkartTest() {
 
-        WebDriver driver = new FirefoxDriver();
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--headless");   // IMPORTANT
+
+        WebDriver driver = new FirefoxDriver(options);
 
         driver.get("https://www.flipkart.com/");
         System.out.println("Title: " + driver.getTitle());
