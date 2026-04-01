6f1cfcdc52443ff8b2b6fa76fd9187e77b91858d
14-Prasanna <2k22cse115@kiot.ac.in>
2026-04-01 11:43:06 +0530

    The test is added

--- src/test/java/com/seleinum/Selenium_Basics/AppTest.java
+++ src/test/java/com/seleinum/Selenium_Basics/AppTest.java
@@ -1,36 +1,20 @@
 package com.seleinum.Selenium_Basics;
 
-import java.io.File;
-import java.nio.file.FileSystemNotFoundException;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class AppTest {
 
-    public static void main(String[] args) {
-
+    @Test
+    public void runFlipkartTest() {
 
         WebDriver driver = new FirefoxDriver();
-        
+
         driver.get("https://www.flipkart.com/");
-        System.out.println("The Page title: " + driver.getTitle());
-                
-        System.out.println("The page URL: " + driver.getCurrentUrl());
-        
-       
-        
-        
+        System.out.println("Title: " + driver.getTitle());
+        System.out.println("URL: " + driver.getCurrentUrl());
 
-        
-
-
-
-        
+        driver.quit();
     }
 }
